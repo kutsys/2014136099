3d9b735e8531a01f87686c15337043f26440c53e
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-12-05 09:41:55 +0900

    Update DemoHW02DirScan.c

--- DemoHW02DirScan.c
+++ DemoHW02DirScan.c
@@ -1,77 +1,57 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <string.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <stdlib.h>
-#include <time.h>
 
-int main(int argc , char *argv[])
+void printdir(char *dir , int depth)
 {
-  char block[1024];
-   char *Original;
-  char *Duplication;
-  int in , out;
-  int nread;
-  clock_t start, end, buffer;
+	DIR *dp;
+	struct dirent *entry;
+	struct stat statbuf;
 
-  if(argc > 3)
-  {
-    printf("입력 초과.\n");
-    printf("프로그램 종료.\n");
-    return;
-  }
-
-  if(argc <= 1 || argv[1] == NULL)
-  {
-    printf("복사 대상 파일 이름이 입력되지 않았습니다.\n");
-    printf("프로그램 종료.\n");
-    exit(1);
-   }
-
-  else
-  {
-      printf("복사 대상 파일 : '%s'\n ",argv[1]);
-      Original = argv[1]; 
-   }
-
-  if(argc <= 2 || argv[2] == NULL)
-  {
-    printf("복사된 파일 이름이 입력되지 않았습니다. \n");
-    printf("'Duplication' 이름을 사용합니다.");
-    Duplication = "Duplication";
-  }
-
-  else
-  {
-      printf("복사된 파일 이름을 입력받았습니다. \n");
-      printf("'%s'이름을 사용합니다\n" , argv[2]);
-      Duplication = argv[2];
-  }
-
-  in = open(Original , O_RDONLY);
-  out = open(Duplication , O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
-  printf("File open error\n");
-  exit(1);
-  }
-  
-  printf("복사를 시작합니다.\n");
-
-  start = clock();
-  buffer = clock();
-
-  while((nread = read(in , block , sizeof(block))) > 0)
-  {
-    write(out , block , nread);
-    printf("*");
-    end = clock();
-    if(end - start - buffer >= 1)
-    {
-    printf("*");
-    }
-    buffer = end;
-  }
+	if((dp = opendir(dir)) == NULL)
+	{
+		fprintf(stderr , " cannot open directory: %s\n", dir);
+		return;
+	}
+	
+	chdir(dir);
  
- close(in);
- close(out);
- printf("\n");
- return(0);
+	while((entry = readdir(dp)) != NULL)  
+	{
+		lstat(entry->d_name , &statbuf);
+		if(S_ISDIR(statbuf.st_mode))
+		{
+			if(strcmp("." , entry->d_name) == 0 || strcmp(".." , entry->d_name) == 0) 
+				continue;
+				if(entry->d_name[0] == '.') 
+				{
+					continue;
+				}				
+			printf("%*s%s\n" , depth, "" ,entry->d_name);		
+		}			
+		else
+		{ 
+			if(entry->d_name[0] == '.') 
+			{
+				continue;
+			}
+			printf("%*s%s\n", depth, "", entry->d_name);
+		}		
+	}
+
+	chdir("..");
+	closedir(dp);
+}			
+	
+
+int main()
+{
+
+	printf("Directory scan : 'present directory'\n");
+	printdir("." , 0); 
+	printf("done.\n");
+	exit(0);
 }
